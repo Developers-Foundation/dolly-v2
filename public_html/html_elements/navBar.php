668610a96146236a2f9f862d1baa6207119d0c03
psalv <psalvato@uwo.ca>
2017-06-15 22:13:10 -0400

    Header LESS

--- public_html/html_elements/navBar.php
+++ public_html/html_elements/navBar.php
@@ -3,52 +3,58 @@
 
 <nav class="navbar navbar-default navbar-fixed-top navbar-custom grey white-text inset">
     <div class="row">
-        <div class="col-xs-6">
 
-        </div>
-        <div class="col-xs-6">
-            <div>
-                <ul class="list-inline text-right underline-white nav-contact-info">
-                    <li><i class="fa fa-envelope-o"></i>dollychildrenfoundationng@gmail.com</li>
-                    <li><i class="fa fa-phone"></i>234-818-689-1611</li>
-                    <li><i class="fa fa-facebook-square"></i>Facebook</li>
-                </ul>
-<!--                <hr>-->
+        <div class="col-sm-3">
+            <div class="row">
+
+                <div class="navbar-header page-scroll">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                    </button>
+
+                    <!--   TODO : How do we want this? Wouldn't a logo with the name like in the footer be better?         -->
+                    <a class="navbar-brand" href="#page-top"><img class="img img-responsive" src="//via.placeholder.com/300x75"/></a>
+
+                </div>
             </div>
         </div>
-    </div>
-    <div class="row">
 
-        <div class="navbar-header page-scroll">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
-            </button>
+        <div class="col-sm-9">
+            <div class="row">
+                <ul class="list-inline text-right">
+                    <li class="li-wrapper">
+                        <ul class="list-inline text-right  nav-contact-info">
+                            <li><i class="fa fa-envelope-o"></i>dollychildrenfoundation@gmail.com</li>
+                            <li><i class="fa fa-phone"></i>234-818-689-1611</li>
+                            <li><i class="fa fa-facebook-square"></i>Facebook</li>
+                        </ul>
+                    </li>
 
-            <a class="navbar-brand" href="#page-top"><img class="navbar-brand-star" /><p class="navbar-brand-text">Dolly Children Foundation</p></a>
-        </div>
+                </ul>
+            </div>
 
-
-        <div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav navbar-right text-uppercase nav-options">
-                <li class="hidden">
-                    <a href="#page-top"></a>
-                </li>
-                <li class="">
-                    <a href="#">About</a>
-                </li>
-                <li class="">
-                    <a href="#">Impacts</a>
-                </li>
-                <li class="">
-                    <a href="#">Blog</a>
-                </li>
-                <li class="">
-                    <a href="#">Contact</a>
-                </li>
-                <li class="">
-                    <a href="">Donate</a>
-                </li>
-            </ul>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav navbar-right text-uppercase nav-options">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="">
+                        <a href="#">About</a>
+                    </li>
+                    <li class="">
+                        <a href="#">Impacts</a>
+                    </li>
+                    <li class="">
+                        <a href="#">Blog</a>
+                    </li>
+                    <li class="">
+                        <a href="#">Contact</a>
+                    </li>
+                    <li class="donate-button">
+                        <a href="">Donate</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </nav>
