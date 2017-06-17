6b9d9f4588d477b51cea7daf4ff5d54534598de5
Nobody Random <iamnobodyrandom@yahoo.com>
2017-06-18 00:08:24 +0800

    add some more content

--- public_html/html_elements/donate.php
+++ public_html/html_elements/donate.php
@@ -10,11 +10,19 @@
 <div class="container">
     <!-- DONATE SECTION -->
     <div class="section">
-        <h2>Always Look On The Bright Side Of Life</h2>
-        <p>From this moment on, choose not to let your past dictate your future. What is gone is gone – forever. Now is the time to move on, to do and become what you.</p>
+        <div class="text-center">
+            <h2>Always Look On The Bright Side Of Life</h2>
+            <div class="row">
+                <div class="col-md-offset-2 col-md-8">
+                    <p>From this moment on, choose not to let your past dictate your future. What is gone is gone –
+                        forever. Now is the time to move on, to do and become what you.</p>
+                </div>
+            </div>
+        </div>
         <div class="card">
-            <form>
+            <form class="donate-form" action="" method="post">
 
+                <button class="donate-button">Donate Now</button>
             </form>
         </div>
     </div>
