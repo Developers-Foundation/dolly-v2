3ff1f951348b4674718d3d2d75415b45f469598c
Nobody Random <iamnobodyrandom@yahoo.com>
2017-06-17 21:15:41 +0800

    about us content

--- public_html/html_elements/about.php
+++ public_html/html_elements/about.php
@@ -13,14 +13,70 @@
         <img src="//via.placeholder.com/1140x482" class="section-bg">
         <div class="row inner">
             <div class="col-md-offset-4 col-md-4 text-center">
-                <h1>Our Mission</h1>
+                <h2>Our Mission</h2>
                 <p>Now, if you are interested in being the best player, getting really good money and knowing some tricks and advices of what to do in a live tournament games, here is the best place to learn them.</p>
             </div>
         </div>
     </div>
 
     <!-- VALUES SECTION -->
-    <div class="section">
+    <div class="section values">
+        <hr class="star-custom">
+        <div class="row text-center">
+            <h2>Values</h2>
+            <div class="col-md-3 card">
+                <div class="icon">
+                    <img src="//via.placeholder.com/86x86" />
+                </div>
+                <h3>Integrity</h3>
+                <p>All the rumors have finally died down and many skeptics have tightened their lips</p>
+            </div>
+            <div class="col-md-3 card center-line">
+                <div class="icon">
+                    <img src="//via.placeholder.com/86x86" />
+                </div>
+                <h3>Commitment</h3>
+                <p>All the rumors have finally died down and many skeptics have tightened their lips</p>
+            </div>
+            <div class="col-md-3 card center-line">
+                <div class="icon">
+                    <img src="//via.placeholder.com/86x86" />
+                </div>
+                <h3>Accountability</h3>
+                <p>All the rumors have finally died down and many skeptics have tightened their lips</p>
+            </div>
+            <div class="col-md-3 card center-line">
+                <div class="icon">
+                    <img src="//via.placeholder.com/86x86" />
+                </div>
+                <h3>Nurturing</h3>
+                <p>All the rumors have finally died down and many skeptics have tightened their lips</p>
+            </div>
+        </div>
+    </div>
 
+    <!-- FOUNDER SECTION -->
+    <div class="section founder">
+        <hr class="star-custom">
+        <div class="row">
+            <h2 class="text-center">Meet the Founder</h2>
+            <div class="col-md-5">
+                <h3>Roxie Saunders</h3>
+                <p><b>Her Message:</b></p>
+                <p>“Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker, projector, hardware and more. laptop accessory compaq laptop battery compaq laptop battery charger compaq laptop computer … Large main compartment features a padded laptop computer pocket, accessory pockets, mesh disk pockets, and ample room for business papers. Right here you can find the best laptop, monitor, printer, PDA, computer component and accessory.”</p>
+            </div>
+            <div class="col-md-offset-1 col-md-6">
+                <img src="//via.placeholder.com/417x282" />
+            </div>
+        </div>
+    </div>
+
+    <!-- VISION SECTION -->
+    <div class="section vision">
+        <hr class="star-custom">
+        <div class="row text-center">
+            <h2>Our Vision</h2>
+            <p>Many people were hoping that if the Democrats won control of Congress they would reverse the online gambling ban, but experts doubt they will even try or that if they do that the will be successful. Once the bill was passed and signed into law by the president, it became much harder to reverse the law.</p>
+        </div>
     </div>
 </div>
