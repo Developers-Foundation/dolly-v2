f58ac495cabe2bb6d8c91c9b669c255fe9d2f3ed
Nobody Random <iamnobodyrandom@yahoo.com>
2017-06-17 19:07:27 +0800

    init about us page

---
+++ public_html/html_elements/about.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: harrisonchow
+ * Date: 6/17/17
+ * Time: 5:08 PM
+ */
+?>
+
+<div class="container">
+    <!-- Reading Clubs -->
+    <div id="reading-clubs" class="section first">
+        <img src="//via.placeholder.com/1140x540" class="section-bg">
+        <div class="row inner">
+            <div class="col-md-offset-2 col-md-4 text-left">
+                <h1>Reading Clubs</h1>
+                <p>
+                    Inspiring the African Child through educational aids and interventions centered on mental and
+                    intellectual programs.
+                </p>
+                <a href class="button-red round">Find out more</a>
+            </div>
+        </div>
+    </div>
+</div>
