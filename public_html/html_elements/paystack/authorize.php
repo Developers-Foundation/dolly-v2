c53c494a1bc4641b01998076e2417970a367025b
Nobody Random <iamnobodyrandom@yahoo.com>
2017-06-27 22:48:17 +0800

    update to plugin

--- public_html/html_elements/paystack/authorize.php
+++ public_html/html_elements/paystack/authorize.php
@@ -6,34 +6,19 @@
  * Time: 10:07 PM
  */
 
+require '../../../vendor/autoload.php';
+
 $secret = $_ENV["PAYSTACK_PRIV_KEY"];
 $email = $_POST["EMAIL"];
 $amount = $_POST["AMOUNT"];
 // TODO: Make it alphanumeric and store to db
 $token = bin2hex(random_bytes(16));
 
-$result = array();
-//Set other parameters as keys in the $postdata array
-$postdata =  array('email' => $email, 'amount' => $amount, "reference" => $token);
-$url = "https://api.paystack.co/transaction/initialize";
+$paystack = new Paystack($secret);
+$responseObj = $paystack->transaction->initialize([
+    "reference"=> $token,
+    "amount"=> $amount,
+    "email"=> $email,
+]);
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postdata));  //Post Fields
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-$headers = [
-    'Authorization: Bearer ' . $secret,
-    'Content-Type: application/json'
-];
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-$request = curl_exec ($ch);
-
-curl_close ($ch);
-
-if ($request) {
-    $result = json_decode($request, true);
-    echo json_encode($result);
-}
+echo $responseObj;
