b6fc9e08b8f53d36c3786828e992b803551c6d65
Nobody Random <iamnobodyrandom@yahoo.com>
2017-08-08 00:01:46 +0800

    should fix?

--- public_html/assets/js/main.js
+++ public_html/assets/js/main.js
@@ -124,6 +124,7 @@ $(document).ready(function () {
 
             $('.form-input-amount')[0].value = amountSelected;
             $('.donate-amount-value').text('Amount ' + amountSelected + ' USD');
+
         });
     }
     /*
